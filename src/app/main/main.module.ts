515ebe36dfbd83f6d25316df24c38754d4d46925
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-09 18:00:15 +0200

    icon

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { NavComponent } from './nav/nav.component';
 import { RouterModule } from "@angular/router";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { RouterModule } from "@angular/router";
   imports: [
     CommonModule,
     RouterModule,
+    FontAwesomeModule,
   ]
 })
 export class MainModule { }
