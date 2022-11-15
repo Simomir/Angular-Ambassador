e9bc9f2b708bb2790ef7450836fe07da25eb7db8
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-15 18:16:14 +0200

    refactoring imported modules

--- src/app/main/secure/secure.module.ts
+++ src/app/main/secure/secure.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecureComponent } from './secure.component';
+import { ProfileComponent } from "./profile/profile.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [
-    SecureComponent
+    SecureComponent,
+    ProfileComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ]
 })
 export class SecureModule { }
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,8 @@
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 
 
 
@@ -15,10 +13,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     RegisterComponent
   ],
   imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class PublicModule { }
