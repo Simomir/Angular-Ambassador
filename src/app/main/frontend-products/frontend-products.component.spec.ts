065232db6bda51e7fa4292601d654b4430f19525
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-15 18:28:25 +0200

    new component

---
+++ src/app/main/frontend-products/frontend-products.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FrontendProductsComponent } from './frontend-products.component';
+
+describe('FrontendProductsComponent', () => {
+  let component: FrontendProductsComponent;
+  let fixture: ComponentFixture<FrontendProductsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FrontendProductsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FrontendProductsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
