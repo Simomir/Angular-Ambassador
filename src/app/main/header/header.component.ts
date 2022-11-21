91a3b20b3ad424797f27abb05d244255fd9d806e
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-21 15:40:55 +0200

    stuff

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Emitters } from "../../emitters/emitters";
+import { User } from "../../interfaces/user";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  title: string = 'Welcome';
+  description: string = 'Share links to earn money';
+  dollarSign = faDollarSign;
 
   constructor() { }
 
   ngOnInit(): void {
+    Emitters.authEmitter.subscribe({
+      next: (user: User) => {
+        if(user) {
+          this.title = `${user.revenue}`;
+          this.description = 'You have earned this far.'
+        }
+      }
+    });
   }
 
 }
