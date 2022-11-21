5a84686269f81c933fd09f83e645039cda0c6887
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-21 17:35:39 +0200

    service logic

--- src/app/services/stats.service.ts
+++ src/app/services/stats.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  stats(): Observable<any> {
+    return this.http.get(`${environment.api}/stats`);
+  }
 }
