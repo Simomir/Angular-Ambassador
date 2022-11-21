3291b7d7e4b1f51071ab4f863be318c3ccf6b8c6
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-21 16:20:24 +0200

    changed the emmiter

--- src/app/emitters/emitters.ts
+++ src/app/emitters/emitters.ts
@@ -2,5 +2,15 @@ import { EventEmitter } from "@angular/core";
 import { User } from "../interfaces/user";
 
 export class Emitters {
+  static _user: User;
   static authEmitter = new EventEmitter<User|null>()
+
+  static set user(user: User) {
+    this._user = user;
+    this.authEmitter.emit(user);
+  }
+
+  static get user(): User {
+    return this._user;
+  }
 }
