4bd70a30fed01084ccf9b3b696fbdd3a86418813
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-22 11:41:37 +0200

    logic

--- src/app/main/secure/rankings/rankings.component.ts
+++ src/app/main/secure/rankings/rankings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StatsService } from "../../../services/stats.service";
 
 @Component({
   selector: 'app-rankings',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rankings.component.css']
 })
 export class RankingsComponent implements OnInit {
+  rankings: any[] = [];
 
-  constructor() { }
+  constructor(private statsService: StatsService) { }
 
   ngOnInit(): void {
+    this.statsService.rankings().subscribe({
+      next: value => {this.rankings = value;}
+    });
   }
 
 }
