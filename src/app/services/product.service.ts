48c870f86d41186e91af94454760fe8828a4560f
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-11-22 16:32:39 +0200

    service logic

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Product } from "../interfaces/product";
 
@@ -12,8 +12,14 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  backend(): Observable<{data: Product[]}> {
-    return this.http.get<{data: Product[]}>(`${this.endpoint}/backend`)
+  backend(filters?: {page?: number}): Observable<{data: Product[]}> {
+    let params = new HttpParams();
+
+    if(filters?.page) {
+      params = params.set('page', filters.page)
+    }
+
+    return this.http.get<{data: Product[]}>(`${this.endpoint}/backend`, {params});
   }
 
 }
